6bff11dfcb0384790f9a2d8e17843fdce00c04d4
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-01-19 11:52:24 -0500

    fix: aes 256 ctr decrypt long text fix

--- include/base64.h
+++ include/base64.h
@@ -7,6 +7,10 @@
 
 namespace base64
 {
+    static const std::string base64_chars =
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+             "0123456789+/=";
     std::string base64_encode(unsigned char const* buf, unsigned int bufLen);
     std::vector<unsigned char> base64_decode(std::string const& encoded_string);
 }
