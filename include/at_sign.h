3d311aacb8c830702e6a226ef4f7df4adf945194
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-02-06 17:05:51 -0500

    feat: `at_sign.h`

--- include/at_sign.h
+++ include/at_sign.h
@@ -3,19 +3,29 @@
 
 #include <string>
 
-class AtSign {
+class AtSign
+{
 
 private:
-    std::string at_sign;
+    std::string at_sign; // e.g. "@bob"
 
 public:
-    AtSign(const std::string &at_sign) : at_sign(at_sign) {}
+    AtSign(const std::string &at_sign) : at_sign(at_sign)
+    {
+        // prepend '@' if it does not exist
+        if (at_sign[0] != '@')
+        {
+            this->at_sign = "@" + at_sign;
+        }
+    }
 
-    std::string get_value() const {
+    std::string get_value() const
+    {
         return at_sign;
     }
 
-    std::string without_prefix() const {
+    std::string without_prefix() const
+    {
         return at_sign.substr(1);
     }
 };
