02932edc343a7d0819bf4ac651c393754eaf64f6
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-02-13 09:29:57 -0500

    feat: send data example

--- examples/at_client_example.cpp
+++ examples/at_client_example.cpp
@@ -1,7 +1,7 @@
 
 #include "at_client.h"
 
-void reading_data()
+void read_data()
 {
     const AtSign esp = AtSign("@soccer0");
     std::map<std::string, std::string> keys = keys_reader::read_keys(esp);
@@ -15,5 +15,30 @@ void reading_data()
     {
         data = at_client->get_ak(at_key);
         std::cout << "data: " << data << std::endl;
+    }
+}
+
+void send_data()
+{
+    const AtSign esp = AtSign("@soccer0");
+    std::map<std::string, std::string> keys = keys_reader::read_keys(esp);
+    AtClient *at_client = new AtClient(esp, keys);
+    at_client->pkam_authenticate();
+
+    const auto java = AtSign("@tastelessbanana");
+    const auto at_key = AtKey("test_esp32", &esp, &java);
+    std::string data;
+    std::string value = "0";
+    for (int i = 0; i < 500; i++)
+    {
+        if(value == "0")
+        {
+            value = "1";
+        }
+        else
+        {
+            value = "0";
+        }
+        at_client->put_ak(at_key, value);
     }
 }
