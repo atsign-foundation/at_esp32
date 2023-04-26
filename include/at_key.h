d68ea217285ed691e5d739584904beda90e0f472
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-04-26 13:03:56 -0400

    feat: made namespace non const

--- include/at_key.h
+++ include/at_key.h
@@ -86,7 +86,7 @@ class AtKey
 
 public:
     const std::string key;
-    const std::string namespace_str;
+    std::string namespace_str;
     const AtSign *shared_by; // the atSign that this key is sharedBy (creator)
     const AtSign *shared_with; // the atSign that this key is sharedWith (could be nullptr)
     Metadata *metadata;
